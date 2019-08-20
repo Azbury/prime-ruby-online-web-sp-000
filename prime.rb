4b24aabfaa363adaa963252b0efeeb8487fd06c6
Austin Asbury <acasbury25@aol.com>
2019-08-20 18:42:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,9 @@ def prime? (num)
   if num == 2 || num == 3 || num == 5 || num == 7
     true
   else
-    if num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0
+    if num < 0
+      false
+    elsif num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0
       false
     else
       true
